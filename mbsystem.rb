44e307e8c0f3794683a652235ea75ccad0981d8e
dwcaress <caress@mbari.org>
2019-01-31 00:58:40 -0800

    Reset gv dependency to new non-core formula
    Reset gv dependency to new non-core formula denismm/gv/gv

--- mbsystem.rb
+++ mbsystem.rb
@@ -10,10 +10,10 @@ class Mbsystem < Formula
   depends_on "netcdf"
   depends_on "proj"
   depends_on "fftw"
-  depends_on "gv"
+  depends_on "denismm/gv/gv"
   depends_on "openmotif"
   depends_on "dwcaress/mbsystem/otps"
-  option "without-check", "Disable build time checks (not recommended)"
+  option "withgvout-check", "Disable build time checks (not recommended)"
   conflicts_with 'dwcaress/mbsystem/mbsystem-beta', :because => 'mbsystem and mbsystem-beta share the same commands'
 
   def install
