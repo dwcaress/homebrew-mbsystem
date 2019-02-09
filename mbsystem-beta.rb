66b9d1c069427593bdc955bd0c1bb85f2d13ed9a
dwcaress <caress@mbari.org>
2019-02-09 11:23:28 -0800

    Update mbsystem-beta.rb

--- mbsystem-beta.rb
+++ mbsystem-beta.rb
@@ -1,7 +1,7 @@
 class MbsystemBeta < Formula
   desc "MB-System seafloor mapping software (Homebrew formula for test distributions)"
   homepage "http://www.mbari.org/products/research-software/mb-system/"
-  url ""
+  url "https://github.com/dwcaress/MB-System/archive/5.7.3.tar.gz"
   sha256 "42e8504e48ccfd7ae9b37fc6bda8b4d264156f617fc6626cad50779875bc6b67"
 
   depends_on :x11
@@ -9,7 +9,7 @@ class MbsystemBeta < Formula
   depends_on "gdal"
   depends_on "netcdf"
   depends_on "proj"
-  depends_on "fftw"ftp://mbsystemftp@ftp.mbari.org/MB-System-5.7.3.tar.gz
+  depends_on "fftw"
   depends_on "openmotif"
   depends_on "dwcaress/mbsystem/otps"
   option "without-check", "Disable build time checks (not recommended)"
