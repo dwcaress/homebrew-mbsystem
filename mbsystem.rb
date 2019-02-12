437dec1b7be04f926075f82066c8b3030c24aa3a
dwcaress <caress@mbari.org>
2019-02-11 23:25:36 -0800

    Update mbsystem.rb

--- mbsystem.rb
+++ mbsystem.rb
@@ -1,8 +1,8 @@
 class Mbsystem < Formula
   desc "MB-System seafloor mapping software"
   homepage "http://www.mbari.org/products/research-software/mb-system/"
-  url "https://github.com/dwcaress/MB-System/archive/5.7.3.tar.gz"
-  sha256 "53fbfbc5f9917725d978c9e4944fc503e07f3a40ca37fd70fad86e2b0548cb50"
+  url "https://github.com/dwcaress/MB-System/archive/5.7.4.tar.gz"
+  sha256 "1faae45b733d3d5273457b2d5801c4241c2fae9a464567764c2505754b5fa1e2"
 
   depends_on :x11
   depends_on "gmt"
