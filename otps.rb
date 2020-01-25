45f3f2be553cdb5b398b3009d8fbc2b12f00fda2
David Caress <caress@mbari.org>
2020-01-25 14:52:59 -0800

    Update otps.rb

--- otps.rb
+++ otps.rb
@@ -16,21 +16,26 @@ class Otps < Formula
 
   def caveats
     <<~EOS
-      This formula has built the 2018 version of OTPS software from the Oregon State University
-      Tide Group in the directory /usr/local/Cellar/otps/2018/
-      and made a link named /usr/local/opt/otps to this directory. This sofware is described at:
+    This formula has built the 2018 version of OTPS software from the Oregon
+    State University Tide Group in the directory 
+        /usr/local/Cellar/otps/2018/
+    and made a link named /usr/local/opt/otps to this directory. This software
+    is described at:
         https://www.tpxo.net/otps
-      In the past, this private Homebrew formula installed the TPXO8_atlas_v1
-      tidal model along with the software. However, as of mid-2019 the Oregon State University tide group no longer makes 
-      their TPXO tidal models openly available for download. Consequently, this formula now installs 
-      the OTPS software required for the MB-System program mbotps to work, but not the associated 
-      tidal model. The program mbotps is currently set up to work with the TPXO9-atlas global tide model,
-      which is their latest 1/30 degree resolution fully global solution, obtained by combining 
-      a 1/6 degree base global solution (TPXO9.v1) with thirty 1/30 degree resolution local solutions 
-      covering all coastal areas, including the Arctic and the Antarctic. 
-      Academic users can register and request access to the TPXO9-atlas model files at this website:
+    In the past, this private Homebrew formula installed the TPXO8_atlas_v1
+    tidal model along with the software. However, as of mid-2019 the Oregon 
+    State University tide group no longer makes their TPXO tidal models openly 
+    available for download. Consequently, this formula now installs  the OTPS 
+    software required for the MB-System program mbotps to work, but not the 
+    associated tidal model. The program mbotps is currently set up to work 
+    with the TPXO9-atlas global tide model,which is their latest 1/30 degree 
+    resolution fully global solution, obtained by combining  a 1/6 degree base 
+    global solution (TPXO9.v1) with thirty 1/30 degree resolution local solutions 
+    covering all coastal areas, including the Arctic and the Antarctic. 
+    Academic users can register and request access to the TPXO9-atlas model 
+    files at this website:
         https://www.tpxo.net
-      There are 25 model files:
+    There are 25 model files:
         grid_tpxo9_atlas_30
         h_2n2_tpxo9_atlas_30
         h_k1_tpxo9_atlas_30
@@ -56,9 +61,11 @@ class Otps < Formula
         u_p1_tpxo9_atlas_30
         u_q1_tpxo9_atlas_30
         u_s2_tpxo9_atlas_30
-      The h_*_tpxo9_atlas_30 and grid_tpxo9_atlas_30 files are 47 MB each, and the u_*_tpxo9_atlas_30 files are 93 MB each.
-      These files should all be placed into the directory /usr/local/opt/otps/DATA/ - once these files are present, the 
-      combination of mbotps and opts/predict_tide should work. See the mbotps manual page for details.
+    The h_*_tpxo9_atlas_30 and grid_tpxo9_atlas_30 files are 47 MB each, and 
+    the u_*_tpxo9_atlas_30 files are 93 MB each. These files should all be 
+    placed into the directory /usr/local/opt/otps/DATA/ - once these files 
+    are present, the combination of mbotps and opts/predict_tide should work. 
+    See the mbotps manual page for details.
     EOS
   end
 
