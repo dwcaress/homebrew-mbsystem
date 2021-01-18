0c6b4b0ceedbdcb2b79c0e49bf0e58811ff49822
David Caress <caress@mbari.org>
2021-01-17 21:52:01 -0800

    Update mbsystem-betamax.rb

--- mbsystem-betamax.rb
+++ mbsystem-betamax.rb
@@ -1,7 +1,7 @@
 class MbsystemBetamax < Formula
   desc "MB-System seafloor mapping software (Homebrew formula for test distributions)"
   homepage "http://www.mbari.org/products/research-software/mb-system/"
-  url "https://github.com/dwcaress/MB-System/archive/5.7.8beta02.tar.gz"
+  url "https://github.com/dwcaress/MB-System/archive/5.7.8.tar.gz"
   sha256 "12c8e70b41c5ae7761757097ff89f97055b5cd32627e2ad05d2c97edb3b0486c"
   license "GPL3"
   
