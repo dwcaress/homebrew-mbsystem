5a984cb8320a15c7e0b16dd2ad1f1f79454f4fda
David Caress <caress@mbari.org>
2021-12-05 17:56:57 -0800

    Update mbsystem-betatrn.rb

--- mbsystem-betatrn.rb
+++ mbsystem-betatrn.rb
@@ -2,7 +2,7 @@ class MbsystemBetatrn < Formula
   desc "MB-System seafloor mapping software (Homebrew formula for test distributions)"
   homepage "http://www.mbari.org/products/research-software/mb-system/"
   url "https://github.com/dwcaress/MB-System/archive/5.7.9beta22.tar.gz"
-  sha256 "28e3c3562d917b304391d0a7f57d4d41eadb473fb5ec8edb8d416295f4f1926b"
+  sha256 "92c5de70c1c9f1dc68f9b9c013035f770a612cc1f13ccffb1f91ed832dbb927f"
   license "GPL3"
   
   depends_on "gmt"
