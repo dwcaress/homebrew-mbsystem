3ddd0788b664df540750579229e3b7db4bddee30
David Caress <caress@mbari.org>
2021-12-18 12:09:49 -0800

    Update mbsystem-beta.rb

--- mbsystem-beta.rb
+++ mbsystem-beta.rb
@@ -2,7 +2,7 @@ class MbsystemBeta < Formula
   desc "MB-System seafloor mapping software (Homebrew formula for test distributions)"
   homepage "http://www.mbari.org/products/research-software/mb-system/"
   url "https://github.com/dwcaress/MB-System/archive/5.7.9beta23.tar.gz"
-  sha256 "1190f8453c05a2ecc0bdaf800adc3a565b1e7f2451f3e867ba1a797bbff45c58"
+  sha256 "11d157d478819f1b483adf192516fc2e2c5030fe8b4f404de1f67cc312c46063"
 
   env :std
   
