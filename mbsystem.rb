f18282e025ba9a2ecd72017e719eb367ebcd5f0b
David Caress <caress@mbari.org>
2026-01-27 18:19:08 -0800

    Update mbsystem.rb

--- mbsystem.rb
+++ mbsystem.rb
@@ -1,7 +1,7 @@
 class Mbsystem < Formula
   desc "MB-System seafloor mapping data processing software"
   homepage "https://www.mbari.org/technology/mb-system/"
-  url "https://github.com/dwcaress/MB-System/archive/refs/tags/MB-System-5.8.3beta12.tar.gz"
+  url "https://github.com/dwcaress/MB-System/archive/refs/tags/5.8.3beta12.tar.gz"
   sha256 "aa3f7c7f79a933d22ff7410284c3c7de9ba9416c14f271bec231b2307101abad"
   license "GPL-3.0-or-later"
   head "https://github.com/dwcaress/MB-System.git", branch: "master"
